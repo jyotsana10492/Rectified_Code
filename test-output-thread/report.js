522db8515e49bd348a6e46ead5de196c1a796ebb
jyotsana10492 <tojyotsanapanwar@g,mail.com>
2021-02-26 10:26:23 +0530

    build01

---
+++ test-output-thread/report.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([
+  {
+    "id": "cf197ab2-7506-46e7-9870-b2395f6e5934",
+    "feature": "To test the Company functionality of Subsource",
+    "scenario": "Check Company deatails",
+    "start": 1614314915039,
+    "group": 12,
+    "content": "",
+    "tags": "",
+    "end": 1614314945792,
+    "className": "passed"
+  },
+  {
+    "id": "aadfb3f3-6152-442b-be40-0543d562982b",
+    "feature": "To test the Shoppage functionality of Subsource",
+    "scenario": "Check Shop deatails",
+    "start": 1614314915041,
+    "group": 13,
+    "content": "",
+    "tags": "",
+    "end": 1614314950029,
+    "className": "passed"
+  }
+]);
+CucumberHTML.timelineGroups.pushArray([
+  {
+    "id": 12,
+    "content": "Thread[TestNG-PoolService-0,5,main]"
+  },
+  {
+    "id": 13,
+    "content": "Thread[TestNG-PoolService-1,5,main]"
+  }
+]);
+});
